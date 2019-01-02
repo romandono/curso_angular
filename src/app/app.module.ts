9a117e8adbc5eb519e21d2fbbb18d1b05770a5f2
Roman <romandonoperez@gmail.com>
2019-01-02 13:14:34 +0100

    Bloque 3- Directiva ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { EjdirectivangstyleComponent } from './ejdirectivangstyle/ejdirectivangs
 import { EjdirectivangclassComponent } from './ejdirectivangclass/ejdirectivangclass.component';
 import { EjdirectivangforComponent } from './ejdirectivangfor/ejdirectivangfor.component';
 import { EjarrayobjetosComponent } from './ejarrayobjetos/ejarrayobjetos.component';
+import { EjdirectivangswitchComponent } from './ejdirectivangswitch/ejdirectivangswitch.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { EjarrayobjetosComponent } from './ejarrayobjetos/ejarrayobjetos.compone
     EjdirectivangstyleComponent,
     EjdirectivangclassComponent,
     EjdirectivangforComponent,
-    EjarrayobjetosComponent
+    EjarrayobjetosComponent,
+    EjdirectivangswitchComponent
   ],
   imports: [
     BrowserModule,
