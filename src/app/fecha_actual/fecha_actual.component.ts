b6e3c2a68bf928455e984fd88d310953c4dfb0ae
Roman <romandonoperez@gmail.com>
2019-01-04 09:43:57 +0100

    Bloque 4- Creación de Pipe propio

--- src/app/fecha_actual/fecha_actual.component.ts
+++ src/app/fecha_actual/fecha_actual.component.ts
@@ -3,13 +3,11 @@ import { Component } from '@angular/core';
 @Component ({
   selector: 'app-fecha_actual',
   template: `
-    <p>{{ sexo | i18nSelect: encabezamiento }} {{ nombre }}</p>
+    <p>La cotización actual del dolar es de {{ dolareuro | euro }}</p>
   `,
   styleUrls: ['./fecha_actual.component.css']
 })
 
 export class Fecha_ActualComponent {
-  nombre: string = 'Carlos';
-  sexo: string = 'hombre';
-  encabezamiento: any = { 'hombre': 'Estimado', 'mujer': 'Estimada' };
+  dolareuro: number = 0.88;
 }
