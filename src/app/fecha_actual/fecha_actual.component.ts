bd891c9c100d53ba4b62df380213000b35f4b1f9
Roman <romandonoperez@gmail.com>
2019-01-04 09:27:34 +0100

    Bloque 4- Pipe Decimal

--- src/app/fecha_actual/fecha_actual.component.ts
+++ src/app/fecha_actual/fecha_actual.component.ts
@@ -3,13 +3,11 @@ import { Component } from '@angular/core';
 @Component ({
   selector: 'app-fecha_actual',
   template: `
-    <p>{{ ciudad | lowercase }}, {{ hoy | date: 'd-M-y'}} a las {{ hoy | date: 'H:m Z'}}</p>
-    <app-copyright></app-copyright>
+    <p>El resultado es {{ resultado | number: '3.2-2' }}</p>
   `,
   styleUrls: ['./fecha_actual.component.css']
 })
 
 export class Fecha_ActualComponent {
-  hoy = new Date();
-  ciudad: string = 'Madrid';
+  resultado: number = 1.148;
 }
