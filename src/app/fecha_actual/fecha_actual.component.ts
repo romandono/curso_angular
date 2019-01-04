3fdebb66a1c9ca1c8d115dee986d445475ba3cf7
Roman <romandonoperez@gmail.com>
2019-01-04 09:36:50 +0100

    Bloque 4- Pipe i18nSelect

--- src/app/fecha_actual/fecha_actual.component.ts
+++ src/app/fecha_actual/fecha_actual.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 @Component ({
   selector: 'app-fecha_actual',
   template: `
-    <p>La cotización actual del dolar es de {{ dolareuro | currency: 'EUR': true }}</p>
+    <p>{{ sexo | i18nSelect: encabezamiento }} {{ nombre }}</p>
   `,
   styleUrls: ['./fecha_actual.component.css']
 })
 
 export class Fecha_ActualComponent {
-  dolareuro: number = 0.88;
+  nombre: string = 'Carlos';
+  sexo: string = 'hombre';
+  encabezamiento: any = { 'hombre': 'Estimado', 'mujer': 'Estimada' };
 }
