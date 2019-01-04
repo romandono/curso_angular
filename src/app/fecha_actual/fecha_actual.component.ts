f2e80a81cafa224c54a1df524cacf5c9996e42d4
Roman <romandonoperez@gmail.com>
2019-01-04 09:07:09 +0100

    Bloque 4- Pipe Date

--- src/app/fecha_actual/fecha_actual.component.ts
+++ src/app/fecha_actual/fecha_actual.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component ({
   selector: 'app-fecha_actual',
   template: `
-    <p>Madrid, {{ hoy | date: 'd/M/y H:m'}}</p>
+    <p>Madrid, {{ hoy | date: 'd-M-y'}} a las {{hoy | date: 'H:m Z'}}</p>
     <app-copyright></app-copyright>
   `,
   styleUrls: ['./fecha_actual.component.css']
